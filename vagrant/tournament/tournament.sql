a2b76bfbb99b517a42acb81ee90e6dc35d5436b8
pmishra <TrueBeamDeveloper@varian.com>
2015-10-05 21:26:18 -0700

    Add command to connect to database and check if database exists already

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,7 +6,9 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
+\c tournament
 
 CREATE TABLE players ( name TEXT,
                        id SERIAL PRIMARY KEY
