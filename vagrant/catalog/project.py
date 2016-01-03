6d579d1ef96a9f14e6dd6bf1744dd9786e13519d
pmishra <TrueBeamDeveloper@varian.com>
2016-01-02 22:22:33 -0800

    Create an app and add routes corresponding to all urls
    Create an instance of catalog web app. Write all route decorators
    corresponding to CRUD operations for items in the movie catalog. These
    routes respond with corresponding text messages.

---
+++ vagrant/catalog/project.py
@@ -0,0 +1,38 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/categories')
+def showCategories():
+    return "This page will show all movie categories."
+
+
+@app.route('/category/<int:category_id>/movies')
+def showCategory(category_id):
+    return "This page will show all movies in category: %s " % (category_id, )
+
+
+@app.route('/category/<int:category_id>/movie/new')
+def newMovie(category_id):
+    return "This page will create a new movie in category: %d" % (category_id, )
+
+
+@app.route('/category/<int:category_id>/movie/<int:movie_id>/edit')
+def editMovie(category_id, movie_id):
+    return "This page will edit movie: %s in category: %s" % (movie_id, category_id)
+
+
+@app.route('/category/<int:category_id>/movie/<int:movie_id>/delete')
+def deleteMovie(category_id, movie_id):
+    return "This page will delete movie: %s in category: %s" % (movie_id, category_id)
+
+
+@app.route('/category/<int:category_id>/movie/<int:movie_id>')
+def showMovie(category_id, movie_id):
+    return "This page will show descritption of movie: %s in category: %s" % (movie_id, category_id)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
