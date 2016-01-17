d135e0ee50ed4a6011f21baa50b5559af0770c09
pmishra02138 <pmishra02138@gmail.com>
2016-01-17 13:59:42 -0800

    Remove relation between user and category tables
    The key relating cateogry to user class has been deleted. This is
    because of th fact that category class is not 'editable'. In future if
    cateogry is made 'editable' then a relationship between user and
    category class needs to be established

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -17,8 +17,6 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
 
     @property
     def serialize(self):
