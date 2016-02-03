2e90f0b4e99cc90e5299df9f34ac9155bc9d4379
pmishra02138 <pmishra02138@gmail.com>
2016-02-03 09:41:50 -0800

    Remove deleted template files
    Template files are moved to a new directory and the files from previous
    git tree are removed.

--- vagrant/catalog/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, DateField, TextAreaField
-
-class MovieForm(Form):
-    name = StringField('Name')
-    releaseDate = DateField('ReleaseDate')
-    description = TextAreaField('Synopsis')
